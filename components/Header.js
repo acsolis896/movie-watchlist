0c0451466fe5a1d3dd1c074bd18cf862c7f065aa
Andrea Solis <104403741+acsolis896@users.noreply.github.com>
2023-03-16 18:20:20 -0400

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <nav id="header">
+            <h1>Find Your Film</h1>
+            <a href="watchlist.html" id="watchlist-btn">My Watchlist</a>
+        </nav>
+    )
+}
