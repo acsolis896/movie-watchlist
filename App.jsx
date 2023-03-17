885a0fa294c1dd26927a5ff4f29f17c8b468539e
acsolis896 <104403741+acsolis896@users.noreply.github.com>
2023-03-17 09:18:27 -0400

    Changed to new REACT syntax

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import Header from "./components/Header.js"
+import Header from "../components/Header.jsx"
+import Main from "../components/Main.jsx"
 
 export default function App() {
     return (
